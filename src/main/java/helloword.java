157b5e5931825111cfd4490c0fe6469b70a0eaa3
liuhao666 <2416341379@qq.com>
2019-07-29 08:14:36 +0800

    这是第一个仓库

---
+++ src/main/java/helloword.java
@@ -0,0 +1,16 @@
+/**
+ * @Author 24163
+ * @Version v 1.0
+ * @Date 2019/7/29
+ */
+public class helloword {
+    public static void main(String[] args) {
+        System.out.println("大当家哈斯科技的");
+        System.out.println("大当家哈斯科技的");
+        System.out.println("大当家哈斯科技的");
+        System.out.println("大当家哈斯科技的");
+        System.out.println("大当家哈斯科技的");
+        System.out.println("大当家哈斯科技的");
+        System.out.println("大当家哈斯科技的");
+    }
+}
